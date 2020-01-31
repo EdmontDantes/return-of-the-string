525d47d3a761ea16d37f6ffcf2acffd58df82faf
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-01-30 23:59:17 -0500

    3 failed, 10 passed, initials proves to be elusive solutions will keep working until pass all

--- main.js
+++ main.js
@@ -2,10 +2,41 @@
  * YOUR CODE HERE! *
  *******************/
 
+//capitalize function
+function capitalize (thisStrToCapitalize) {
+  return thisStrToCapitalize.toUpperCase();
+}
 
+function exclaim (thisExtraExclainChar) {
+  return thisExtraExclainChar.concat('!');
+}
 
+function firstCharacter (aStrFirstCharGiveback) {
+  return aStrFirstCharGiveback.charAt(0);
+}
 
- 
+function lastCharacter (aStrLastCharGiveback) {
+  return aStrLastCharGiveback.slice(-1);
+}
+
+function oneCharacter (aStrArbitraryCharOne, x) {
+  return aStrArbitraryCharOne.charAt(x);
+}
+
+function twoCharacters (aStrArbitraryCharTwo, x, y) {
+  return aStrArbitraryCharTwo.charAt(x) + aStrArbitraryCharTwo.charAt(y);
+}
+
+function yeller (aStrYeller) {
+  return aStrYeller.toUpperCase() + '!!!';
+}
+
+//for this stretch goal I've used information from : https://stackoverflow.com/questions/33076177/getting-name-initials-using-js
+function initials (fullName) {
+  const separateString = fullName.split(' ');
+  return separateString.substring(0).charAt(0).toUpperCase + '.' + separateString.substring(1).charAt(0).toUpperCase + '.';
+  
+}
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
